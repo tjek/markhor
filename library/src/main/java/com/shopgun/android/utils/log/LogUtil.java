335bec739072c6a210dc4851aea5f639e32c3286
dkhvam@gmail.com <Danny Hvam>
2016-09-05 20:49:27 +0200

    grade and logutil update

--- library/src/main/java/com/shopgun/android/utils/log/LogUtil.java
+++ library/src/main/java/com/shopgun/android/utils/log/LogUtil.java
@@ -2,12 +2,14 @@ package com.shopgun.android.utils.log;
 
 public class LogUtil {
 
+    public static final String TAG = LogUtil.class.getSimpleName();
+    
     /**
      * Print the calling method by name and linenumber.
      * <p>com.shopgun.android.utils.MyClass.myMethod(MyClass.java:87)</p>
      */
     public static void printCallingMethod(Logger logger) {
-        printMethod(logger, 3);
+        printMethod(logger, 4);
     }
 
     /**
@@ -18,14 +20,25 @@ public class LogUtil {
      */
     public static void printMethod(Logger logger, int element) {
         StackTraceElement ste = Thread.currentThread().getStackTrace()[element];
-        String tag = ste.getClass().getSimpleName();
+        String clazzName = ste.getClassName();
+        String tag = clazzName.substring(clazzName.lastIndexOf(".")+1, clazzName.length());
         String msg = ste.getMethodName() + "(" + ste.getFileName() + ":" + ste.getLineNumber() + ")";
         logger.d(tag, msg);
     }
 
     /**
      * Print a StackTrace from any given point of your source code.
-     * @param tag     Used to identify the source of a log message. It usually identifies the class or activity where the log call occurs.
+     * Used to identify the source of a log message. It usually identifies the class or activity where the log call occurs.
+     */
+    public static void printStackTrace(Logger logger) {
+        String tag = Thread.currentThread().getStackTrace()[3].getClass().getSimpleName();
+        printStackTrace(logger, tag);
+    }
+
+    /**
+     * Print a StackTrace from any given point of your source code.
+     * @param tag Used to identify the source of a log message. It usually identifies the class or activity where the log call occurs.
+     * @param logger A logger for output
      */
     public static void printStackTrace(Logger logger, String tag) {
         for (StackTraceElement ste : Thread.currentThread().getStackTrace()) {
