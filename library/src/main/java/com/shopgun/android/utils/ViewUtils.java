2f5933502e1cb29bf4c9474214c603b483cc7285
dkhvam@gmail.com <Danny Hvam>
2016-10-11 13:00:46 +0200

    generate view ids

--- library/src/main/java/com/shopgun/android/utils/ViewUtils.java
+++ library/src/main/java/com/shopgun/android/utils/ViewUtils.java
@@ -16,12 +16,18 @@
 
 package com.shopgun.android.utils;
 
+import android.os.Build;
 import android.view.KeyEvent;
+import android.view.View;
 import android.view.inputmethod.EditorInfo;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class ViewUtils {
 
     public static final String TAG = Tag.from(ViewUtils.class);
+
+    private static final AtomicInteger sNextGeneratedId = new AtomicInteger(1);
 
     private ViewUtils() {
         // private constructor
@@ -34,6 +40,38 @@ public class ViewUtils {
                 (event != null && (
                         event.getAction() == KeyEvent.ACTION_DOWN &&
                                 event.getKeyCode() == KeyEvent.KEYCODE_ENTER));
+    }
+
+    /**
+     * Generate a value suitable for use in {@link View#setId(int)}.
+     * This value will not collide with ID values generated at build time by aapt for R.id.
+     *
+     * @return a generated ID value
+     */
+    public static int generateViewId() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            return generateViewIdPreJellyBeans();
+        } else {
+            return View.generateViewId();
+        }
+    }
+
+    /**
+     * Generate a value suitable for use in {@link View#setId(int)}.
+     * This value will not collide with ID values generated at build time by aapt for R.id.
+     *
+     * @return a generated ID value
+     */
+    private static int generateViewIdPreJellyBeans() {
+        for (;;) {
+            final int result = sNextGeneratedId.get();
+            // aapt-generated IDs have the high byte nonzero; clamp to the range under that.
+            int newValue = result + 1;
+            if (newValue > 0x00FFFFFF) newValue = 1; // Roll over to 1, not 0.
+            if (sNextGeneratedId.compareAndSet(result, newValue)) {
+                return result;
+            }
+        }
     }
 
 }
