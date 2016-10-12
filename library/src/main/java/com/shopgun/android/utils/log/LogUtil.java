9c4229e0e7b0d6ac8584ecd725c2448f8640321a
dkhvam@gmail.com <Danny Hvam>
2016-10-12 09:33:14 +0200

    Log printing

--- library/src/main/java/com/shopgun/android/utils/log/LogUtil.java
+++ library/src/main/java/com/shopgun/android/utils/log/LogUtil.java
@@ -18,12 +18,32 @@ public class LogUtil {
      *
      * @param element argument to easily evaluate it
      */
+    public static void printMethod(int element) {
+        LogUtil.printMethod(L.getLogger(), element);
+    }
+
+    /**
+     * Print the calling method by name and linenumber.
+     * <p>com.shopgun.android.utils.MyClass.myMethod(MyClass.java:87)</p>
+     *
+     * @param element argument to easily evaluate it
+     */
     public static void printMethod(Logger logger, int element) {
         StackTraceElement ste = Thread.currentThread().getStackTrace()[element];
         String clazzName = ste.getClassName();
         String tag = clazzName.substring(clazzName.lastIndexOf(".")+1, clazzName.length());
         String msg = ste.getMethodName() + "(" + ste.getFileName() + ":" + ste.getLineNumber() + ")";
         logger.d(tag, msg);
+    }
+
+
+    /**
+     * Print a StackTrace from any given point of your source code.
+     * Used to identify the source of a log message. It usually identifies the class or activity where the log call occurs.
+     * <p>This method will use the default logger found in {@link L#getLogger()}</p>
+     */
+    public static void printStackTrace() {
+        LogUtil.printStackTrace(L.getLogger());
     }
 
     /**
@@ -33,6 +53,16 @@ public class LogUtil {
     public static void printStackTrace(Logger logger) {
         String tag = Thread.currentThread().getStackTrace()[3].getClass().getSimpleName();
         printStackTrace(logger, tag);
+    }
+
+
+    /**
+     * Print a StackTrace from any given point of your source code.
+     * <p>This method will use the default logger found in {@link L#getLogger()}</p>
+     * @param tag Used to identify the source of a log message. It usually identifies the class or activity where the log call occurs.
+     */
+    public static void printStackTrace(String tag) {
+        LogUtil.printStackTrace(L.getLogger(), tag);
     }
 
     /**
@@ -46,6 +76,26 @@ public class LogUtil {
         }
     }
 
+    /**
+     * Print a StackTrace from any given point of your source code. But only include the lines
+     * given as {@code first} and {@code last} in the arguments.
+     * <p>This method will use the default logger found in {@link L#getLogger()}</p>
+     * @param tag Used to identify the source of a log message. It usually identifies the class or activity where the log call occurs.
+     * @param first first line to print
+     * @param last last line to print
+     */
+    public static void printStackTrace(String tag, int first, int last) {
+        LogUtil.printStackTrace(L.getLogger(), tag, first, last);
+    }
+
+    /**
+     * Print a StackTrace from any given point of your source code. But only include the lines
+     * given as {@code first} and {@code last} in the arguments.
+     * @param logger A logger for output
+     * @param tag Used to identify the source of a log message. It usually identifies the class or activity where the log call occurs.
+     * @param first first line to print
+     * @param last last line to print
+     */
     public static void printStackTrace(Logger logger, String tag, int first, int last) {
         StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
         first = Math.min(first, stackTraceElements.length);
