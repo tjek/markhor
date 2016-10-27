45ee9cab914ef4bebc9a9aaa13ce759c3ca81199
dkhvam@gmail.com <Danny Hvam>
2016-10-27 07:49:26 +0200

    Added memory utils, to get max heap size

---
+++ library/src/main/java/com/shopgun/android/utils/MemoryUtils.java
@@ -0,0 +1,30 @@
+package com.shopgun.android.utils;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.os.Build;
+
+public class MemoryUtils {
+
+    public static final String TAG = MemoryUtils.class.getSimpleName();
+
+    private MemoryUtils() {
+        //no instance
+    }
+    
+    /**
+     * Get the max available heap size
+     *
+     * @param c A context
+     * @return the maximum available heap size for the device
+     */
+    public static int getMaxHeap(Context c) {
+        ActivityManager am = (ActivityManager) c.getSystemService(Context.ACTIVITY_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            return am.getLargeMemoryClass();
+        } else {
+            return am.getMemoryClass();
+        }
+    }
+
+}
