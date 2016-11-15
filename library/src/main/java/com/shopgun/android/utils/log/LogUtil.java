adc1ef2f882881c6dfbf01f0d477744a9ab084c6
dkhvam@gmail.com <Danny Hvam>
2016-11-15 09:50:48 +0100

    Updated LogUtils

--- library/src/main/java/com/shopgun/android/utils/log/LogUtil.java
+++ library/src/main/java/com/shopgun/android/utils/log/LogUtil.java
@@ -3,24 +3,24 @@ package com.shopgun.android.utils.log;
 public class LogUtil {
 
     public static final String TAG = LogUtil.class.getSimpleName();
-    
+
+    private static final int CALLING_ELEMENT = 4;
+
     /**
      * Print the calling method by name and linenumber.
      * <p>com.shopgun.android.utils.MyClass.myMethod(MyClass.java:87)</p>
-     * @param logger A logger for output
      */
-    public static void printCallingMethod(Logger logger) {
-        printMethod(logger, 4);
+    public static void printMethod() {
+        printMethod(L.getLogger(), CALLING_ELEMENT);
     }
 
     /**
      * Print the calling method by name and linenumber.
      * <p>com.shopgun.android.utils.MyClass.myMethod(MyClass.java:87)</p>
-     *
-     * @param element argument to easily evaluate it
+     * @param logger A logger for output
      */
-    public static void printMethod(int element) {
-        LogUtil.printMethod(L.getLogger(), element);
+    public static void printMethod(Logger logger) {
+        printMethod(logger, CALLING_ELEMENT);
     }
 
     /**
@@ -32,12 +32,10 @@ public class LogUtil {
      */
     public static void printMethod(Logger logger, int element) {
         StackTraceElement ste = Thread.currentThread().getStackTrace()[element];
-        String clazzName = ste.getClassName();
-        String tag = clazzName.substring(clazzName.lastIndexOf(".")+1, clazzName.length());
+        String tag = getTagFromElement(ste);
         String msg = ste.getMethodName() + "(" + ste.getFileName() + ":" + ste.getLineNumber() + ")";
         logger.d(tag, msg);
     }
-
 
     /**
      * Print a StackTrace from any given point of your source code.
@@ -54,10 +52,9 @@ public class LogUtil {
      * @param logger A logger for output
      */
     public static void printStackTrace(Logger logger) {
-        String tag = Thread.currentThread().getStackTrace()[3].getClass().getSimpleName();
-        printStackTrace(logger, tag);
+        StackTraceElement[] trace = Thread.currentThread().getStackTrace();
+        printStackTrace(logger, getTagFromTrace(trace), trace);
     }
-
 
     /**
      * Print a StackTrace from any given point of your source code.
@@ -74,7 +71,11 @@ public class LogUtil {
      * @param tag Used to identify the source of a log message. It usually identifies the class or activity where the log call occurs.
      */
     public static void printStackTrace(Logger logger, String tag) {
-        for (StackTraceElement ste : Thread.currentThread().getStackTrace()) {
+        printStackTrace(logger, tag, Thread.currentThread().getStackTrace());
+    }
+
+    private static void printStackTrace(Logger logger, String tag, StackTraceElement[] trace) {
+        for (StackTraceElement ste : trace) {
             logger.d(tag, String.valueOf(ste));
         }
     }
@@ -107,6 +108,27 @@ public class LogUtil {
             StackTraceElement ste = stackTraceElements[i];
             L.d(tag, String.valueOf(ste));
         }
+    }
+
+    /**
+     * Returns a tag for use in a {@link Logger}, in the format of a class name.
+     * E.g.: <p>MyClass</p>
+     * @param trace A stack trace to get the info from
+     * @return A tag
+     */
+    private static String getTagFromTrace(StackTraceElement[] trace) {
+        return getTagFromElement(trace[CALLING_ELEMENT]);
+    }
+
+    /**
+     * Returns a tag for use in a {@link Logger}, in the format of a class name.
+     * E.g.: <p>MyClass</p>
+     * @param element An element to get info from
+     * @return A tag
+     */
+    private static String getTagFromElement(StackTraceElement element) {
+        String[] split = element.getClassName().split("\\.");
+        return split[split.length-1];
     }
 
 }
