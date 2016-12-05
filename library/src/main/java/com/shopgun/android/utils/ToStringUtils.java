c5d53086211de6e64a6b7a497e61cfda8dea2143
dkhvam@gmail.com <Danny Hvam>
2016-12-05 10:06:41 +0100

    Added MeasureSpec strings, and MotionEvent actions

--- library/src/main/java/com/shopgun/android/utils/ToStringUtils.java
+++ library/src/main/java/com/shopgun/android/utils/ToStringUtils.java
@@ -2,7 +2,8 @@ package com.shopgun.android.utils;
 
 import android.content.res.Configuration;
 import android.support.v4.view.ViewPager;
-import android.view.View;
+import android.view.MotionEvent;
+import android.view.View.MeasureSpec;
 
 public class ToStringUtils {
 
@@ -32,10 +33,57 @@ public class ToStringUtils {
      */
     public static String measureSpecMode(int mode) {
         switch (mode) {
-            case View.MeasureSpec.UNSPECIFIED: return "UNSPECIFIED";
-            case View.MeasureSpec.EXACTLY: return "EXACTLY";
-            case View.MeasureSpec.AT_MOST: return "AT_MOST";
+            case MeasureSpec.UNSPECIFIED: return "UNSPECIFIED";
+            case MeasureSpec.EXACTLY: return "EXACTLY";
+            case MeasureSpec.AT_MOST: return "AT_MOST";
             default: return "UNSPECIFIED";
+        }
+    }
+
+    /**
+     * Returns the measurespec in a readable format:
+     * <p>E.g.: MeasureSpec[ w: AT_MOST 1845, h: EXACTLY 987 ]</p>
+     *
+     * @param widthMeasureSpec the measure specification for width based on size and mode
+     * @param heightMeasureSpec the measure specification for height based on size and mode
+     * @return a human readable string representing the MeasureSpec
+     */
+    public static String measureSpec(int widthMeasureSpec, int heightMeasureSpec) {
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+        // MeasureSpec[ w: AT_MOST 1845, h: EXACTLY 987 ]
+        return String.format("MeasureSpec[ w: %s %s, h: %s %s ]",
+                measureSpecMode(widthMode).toLowerCase(), widthSize,
+                measureSpecMode(heightMode).toLowerCase(), heightSize);
+    }
+
+    public static String motionEventAction(MotionEvent ev) {
+        return motionEventAction(ev.getAction() & MotionEvent.ACTION_MASK);
+    }
+
+    public static String motionEventAction(int action) {
+        switch (action) {
+            case MotionEvent.ACTION_DOWN: return "ACTION_DOWN";
+            case MotionEvent.ACTION_UP: return "ACTION_UP";
+            case MotionEvent.ACTION_MOVE: return "ACTION_MOVE";
+            case MotionEvent.ACTION_CANCEL: return "ACTION_CANCEL";
+            case MotionEvent.ACTION_OUTSIDE: return "ACTION_OUTSIDE";
+            case MotionEvent.ACTION_POINTER_DOWN: return "ACTION_POINTER_DOWN";
+            case MotionEvent.ACTION_POINTER_UP: return "ACTION_POINTER_UP";
+            case MotionEvent.ACTION_HOVER_MOVE: return "ACTION_HOVER_MOVE";
+            case MotionEvent.ACTION_SCROLL: return "ACTION_SCROLL";
+            case MotionEvent.ACTION_HOVER_ENTER: return "ACTION_HOVER_ENTER";
+            case MotionEvent.ACTION_HOVER_EXIT: return "ACTION_HOVER_EXIT";
+            case MotionEvent.ACTION_BUTTON_PRESS: return "ACTION_BUTTON_PRESS";
+            case MotionEvent.ACTION_BUTTON_RELEASE: return "ACTION_BUTTON_RELEASE";
+            case MotionEvent.ACTION_POINTER_INDEX_MASK: return "ACTION_POINTER_INDEX_MASK";
+            case MotionEvent.ACTION_POINTER_2_DOWN: return "ACTION_POINTER_2_DOWN";
+            case MotionEvent.ACTION_POINTER_3_DOWN: return "ACTION_POINTER_3_DOWN";
+            case MotionEvent.ACTION_POINTER_2_UP: return "ACTION_POINTER_2_UP";
+            case MotionEvent.ACTION_POINTER_3_UP: return "ACTION_POINTER_3_UP";
+            default: return "ACTION_UNKNOWN";
         }
     }
 
